1977c45c5a99bf75c4f26493859c710e4ea79c2d
Lorenzo Bini <Lorenzo.Bini@unige.ch>
2024-04-24 14:13:24 +0200

    Update graph_generation.py

--- utils/graph_generation.py
+++ utils/graph_generation.py
@@ -1,3 +1,4 @@
+
 '''
 This script generates k-nearest neighbors graphs with k = 5 neighbors.
 '''
@@ -18,30 +19,6 @@ random.seed(seed_value)
 np.random.seed(seed_value)
 torch.manual_seed(seed_value)
 torch.cuda.manual_seed_all(seed_value)
-
-PRINT_MEMORY = False
-device_string = "cuda" if torch.cuda.is_available() else "cpu"
-device = torch.device(device_string)
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:21"
-
-# Check if a GPU is available
-if torch.cuda.is_available():
-    # Get the current GPU device
-    device = torch.cuda.current_device()
-    
-    # Get the GPU's memory usage in bytes
-    memory_allocated = torch.cuda.memory_allocated(device)
-    memory_cached = torch.cuda.memory_cached(device)
-    
-    # Convert bytes to a more human-readable format (e.g., megabytes or gigabytes)
-    memory_allocated_mb = memory_allocated / 1024**2  # Megabytes
-    memory_cached_mb = memory_cached / 1024**2  # Megabytes
-    
-    print(f"GPU Memory Allocated: {memory_allocated_mb:.2f} MB")
-    print(f"GPU Memory Cached: {memory_cached_mb:.2f} MB")
-else:
-    print("No GPU available.")
 
 to_skip = ['root']
 ATTRIBUTE_class = "1,2,3,4,5,5_1,5_2,5_3"
@@ -69,45 +46,39 @@ for i in range(len(AA)):
 R = torch.tensor(R)
 #Transpose to get the descendants for each node 
 R = R.transpose(1, 0)
-R = R.unsqueeze(0).to(device)
+R = R.unsqueeze(0)
 
-K = 5
-knn = NearestNeighbors(n_neighbors=K)
 data_FC=[]
-for j in range(30):
+for j in range(29):
     # Data before min max normalization
     df = pd.read_csv(f"Data_hierarchical/Case_{j+1}.csv",dtype={"label": str})
     actual_labels = df[["label"]]
     labels = df.label.tolist()
+    
     # Extract outputs
     Y = []
     for item in labels:
         y_ = np.zeros(len(nodes))
-        for t in item.split('@'): 
-            y_[[nodes_idx.get(a) for a in nx.ancestors(g_t, t.replace('_', '.'))]] =1
-            y_[nodes_idx[t.replace('_', '.')]] = 1
+        for t in item.split('@'):
+            if t.replace('_', '.') in nodes_idx:  # Check if the node is in the graph
+                y_[[nodes_idx.get(a) for a in nx.ancestors(g_t, t.replace('_', '.'))]] =1
+                y_[nodes_idx[t.replace('_', '.')]] = 1
         Y.append(y_)
     Y = np.stack(Y)
-    #check the type
+    
     df = df.drop('label', axis=1)
-    # Select node features
     x = df[[ 'FS INT',  'SS INT', 'FL1 INT_CD14-FITC', 'FL2 INT_CD19-PE', 'FL3 INT_CD13-ECD', 'FL4 INT_CD33-PC5.5', 'FL5 INT_CD34-PC7', 'FL6 INT_CD117-APC', 'FL7 INT_CD7-APC700', 'FL8 INT_CD16-APC750', 'FL9 INT_HLA-PB', 'FL10 INT_CD45-KO']].to_numpy() # [num_nodes x num_features]
-    knn.fit(x)
-    # Extract edges
-    all_edges = np.array([], dtype=np.int32).reshape((0, 2))
-    for i in range(len(df)):
-        p=knn.kneighbors([x[i]], return_distance=False)[0]
-        # Build all combinations
-        permutations = list(itertools.combinations(p[1:K], 2))
-        edges_source = [e[0] for e in permutations]
-        edges_target = [e[1] for e in permutations]
-        edges = np.column_stack([edges_source, edges_target])
-        all_edges = np.vstack([all_edges, edges])
-    # Convert to Pytorch Geometric format
-    edge_index = all_edges.transpose()
-    edge_index # [2, num_edges]
-    edge_index1 = e.t().clone().detach()
+
+    k_neighbors = 7  # Number of neighbors for k-NN
+    nbrs = NearestNeighbors(n_neighbors=k_neighbors, metric='euclidean').fit(x)
+    distances, indices = nbrs.kneighbors(x)
+    src = np.repeat(np.arange(x.shape[0]), k_neighbors)
+    dst = indices.flatten()
+    edge_index = torch.tensor([src, dst], dtype=torch.long)
+
+    # Min-Max Normalization
     for i in range(x.shape[1]):
-        x[:,i] = (x[:,i] - x[:,i].min()) / (x[:,i].max() - x[:,i].min())
-    data_FC.append(Data(x=torch.tensor(x, dtype=torch.float), edge_index=e.to().contiguous(), y=torch.tensor(Y),A=np.array(nx.to_numpy_array(g, nodelist=nodes)),terms=nodes,g=g,to_eval=evall,yy=list(actual_labels.to_numpy().flatten())))
+        x[:, i] = (x[:, i] - x[:, i].min()) / (x[:, i].max() - x[:, i].min())
+    data_FC.append(Data(x=torch.tensor(x, dtype=torch.float), edge_index=edge_index, y=torch.tensor(Y), A=np.array(nx.to_numpy_array(g, nodelist=nodes)), terms=nodes, g=g, to_eval=evall, yy=list(actual_labels.to_numpy().flatten())))
+
 torch.save(data_FC, 'graph_hierarchical_with_labels.pt')
