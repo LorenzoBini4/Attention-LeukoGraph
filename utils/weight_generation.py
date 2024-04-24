cf62c2973c04ea5522490fc7b18c4d76aad0aa29
Lorenzo Bini <Lorenzo.Bini@unige.ch>
2024-04-24 14:15:29 +0200

    Update weight_generation.py

--- utils/weight_generation.py
+++ utils/weight_generation.py
@@ -1,7 +1,5 @@
 #### Hierarchical Multilabel Classification ###
 '''
-This script generate dataset from FCS files of 30 patients.
-
 The notations correspond to the labels in the following way:
 'O' -> T lymphocytes
 'N' -> B lymphocytes
@@ -22,28 +20,27 @@ p = dict()
 for i in range(1, 7):
     p[i-1] = dict()
     for ch in ['A', 'G', 'K', 'L', 'M', 'N', 'O', 'P']:
-        p[i-1][ch] = FlowCal.io.FCSData(f'/home/users/b/bini/gnn/multiclass/folder1/Case{i}_' + ch + '.fcs')
+        p[i-1][ch] = FlowCal.io.FCSData(f'/home/folder1/Case{i}_' + ch + '.fcs')
         
 for i in range(7, 30):
     p[i-1] = dict()
     for ch in ['A', 'G', 'K', 'L', 'M', 'N', 'O', 'P']:
-        p[i-1][ch] = FlowCal.io.FCSData(f'/home/users/b/bini/gnn/multiclass/folder2/Case{i}_' + ch + '.fcs')
+        p[i-1][ch] = FlowCal.io.FCSData(f'/home/folder2/Case{i}_' + ch + '.fcs')
 
 p[29] = dict()
-p[29]['A'] = np.genfromtxt('/home/users/b/bini/gnn/multiclass/folder2/Case31_A.csv', delimiter=";")[1:]
-p[29]['G'] = np.genfromtxt('/home/users/b/bini/gnn/multiclass/folder2/Case31_G.csv', delimiter=";")[1:]
-p[29]['K'] = np.genfromtxt('/home/users/b/bini/gnn/multiclass/folder2/Case31_K.csv', delimiter=";")[1:]
-p[29]['L'] = np.genfromtxt('/home/users/b/bini/gnn/multiclass/folder2/Case31_L.csv', delimiter=";")[1:]
-p[29]['M'] = np.genfromtxt('/home/users/b/bini/gnn/multiclass/folder2/Case31_M.csv', delimiter=";")[1:]
-p[29]['N'] = np.genfromtxt('/home/users/b/bini/gnn/multiclass/folder2/Case31_N.csv', delimiter=";")[1:]
-p[29]['O'] = np.genfromtxt('/home/users/b/bini/gnn/multiclass/folder2/Case31_O.csv', delimiter=";")[1:]
-p[29]['P'] = np.genfromtxt('/home/users/b/bini/gnn/multiclass/folder2/Case31_P.csv', delimiter=";")[1:]
+p[29]['A'] = np.genfromtxt('/home/folder2/Case31_A.csv', delimiter=";")[1:]
+p[29]['G'] = np.genfromtxt('/home/folder2/Case31_G.csv', delimiter=";")[1:]
+p[29]['K'] = np.genfromtxt('/home/folder2/Case31_K.csv', delimiter=";")[1:]
+p[29]['L'] = np.genfromtxt('/home/folder2/Case31_L.csv', delimiter=";")[1:]
+p[29]['M'] = np.genfromtxt('/home/folder2/Case31_M.csv', delimiter=";")[1:]
+p[29]['N'] = np.genfromtxt('/home/folder2/Case31_N.csv', delimiter=";")[1:]
+p[29]['O'] = np.genfromtxt('/home/folder2/Case31_O.csv', delimiter=";")[1:]
+p[29]['P'] = np.genfromtxt('/home/folder2/Case31_P.csv', delimiter=";")[1:]
 
 # Dataset generation Hierarchical approach with columns O,N,G,P,K,M,L,H
 total_weights=np.zeros((30,8))
 column=('FS INT', 'SS PEAK', 'SS INT', 'SS TOF', 'FL1 INT_CD14-FITC', 'FL2 INT_CD19-PE', 'FL3 INT_CD13-ECD', 'FL4 INT_CD33-PC5.5', 'FL5 INT_CD34-PC7', 'FL6 INT_CD117-APC', 'FL7 INT_CD7-APC700', 'FL8 INT_CD16-APC750', 'FL9 INT_HLA-PB', 'FL10 INT_CD45-KO', 'TIME')
 for i in range(30):
-    
     df_A=pd.DataFrame(p[i]['A'],columns=column)
     df_G=pd.DataFrame(p[i]['G'],columns=column)
     df_K=pd.DataFrame(p[i]['K'],columns=column)
@@ -54,15 +51,10 @@ for i in range(30):
     df_P=pd.DataFrame(p[i]['P'],columns=column)
     
     X = pd.concat([df_M,df_L])
-
     X = X.drop_duplicates()
-
     merged = df_K.merge(X, indicator=True, how='outer')
-
     df_H = merged[merged['_merge'] == 'left_only']
     df_H = df_H.drop(columns=['_merge'])  
-    
-    
     
     df_O['label']="1"
     df_N['label']="2"
@@ -73,9 +65,7 @@ for i in range(30):
     df_L['label']="5_2"
     df_H['label']="5_3"
  
-
     df = pd.concat([df_O,df_N,df_G,df_P,df_K,df_M,df_L,df_H])
-    
     labels = df['label']
     class_weight = compute_class_weight('balanced', classes=np.unique(labels), y=labels)
     total_weights[i,:] = class_weight
